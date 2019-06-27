e69011c3800cecf0d390beb39b72f4c9eba0c3fe
Calvin McLean <calvinmclean@email.arizona.edu>
2019-06-27 15:57:17 -0700

    Remove GetAllRecipes from Client

--- client/Client.go
+++ client/Client.go
@@ -13,11 +13,8 @@ func main() {
 	ing := getIngredient(int32(1))
 	rec := getRecipe(int32(1))
 
-	allRec := getAllRecipes()
-
 	log.Printf("Ingredient 1 name: %s", ing.Name)
 	log.Printf("Recipe 1 name: %s", rec.Name)
-	log.Printf("AllRecipes 0 name: %s", allRec[0].Name)
 }
 
 func getConnection(addr string) *grpc.ClientConn {
@@ -50,20 +47,6 @@ func getRecipe(id int32) pb.Recipe {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	rec, err := c.GetRecipe(ctx, &pb.RecipeRequest{Id: 1})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	return *rec
-}
-
-func getAllRecipes() pb.AllRecipes {
-	conn := getConnection("recipes:50052")
-	defer conn.Close()
-	c := pb.NewRecipesClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	rec, err := c.GetAllRecipes(ctx, &pb.AllRecipesRequest{})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
